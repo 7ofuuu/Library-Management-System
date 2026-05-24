07f450c7df70bec8fb3f2a9ec885839b4f9b2e66
fathanPrayoga <fathan.wicaksono@gmail.com>
2026-05-24 21:08:59 +0700

    ci: sesuaikan logika assertion agar kompatibel dengan state awal objek

--- src/test/java/com/library/LibraryTest.java
+++ src/test/java/com/library/LibraryTest.java
@@ -1,7 +1,7 @@
 package com.library;
 
-
-
+import src.main.java.com.library.Book;
+import src.main.java.com.library.Library;
 
 public class LibraryTest {
     public static void main(String[] args) {
@@ -14,6 +14,8 @@ public class LibraryTest {
         Library library = new Library();
         Book book = new Book("Verity", "Colleen Hoover");
         library.addBook(book);
+        boolean statusBuku = book.isAvailable();
+        assert statusBuku == book.isAvailable() : "ERROR: Status buku tidak konsisten!";
         assert book.isAvailable() == true : "ERROR: Buku baru harusnya berstatus Available!";
         System.out.println("[PASSED] testAddBook");
     }
